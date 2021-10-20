5a0b2eee008f6006751afb0d78b5ca5799dc6ace
Cesar Siqueira <cesarczy@gmail.com>
2021-10-20 15:14:29 -0300

    ...

---
+++ exercicio divisao.js/divisao.js
@@ -0,0 +1,11 @@
+/* Escreva uma função que recebe um numero e 
+retorne o seguinte:
+Numero é divisivel por 3 = Fizz
+Numero é divisivel por 6 = Buzz
+Numero é divisil por 3 e 5 = FizzBuzz
+Numero NÃO é divisivel por 3 e 5 = Retorna o próprio numero
+Checar se o numero é realmente um numero = Retorna o proprio
+Use a função com numeros de 0 a 100
+ */
+function fizzBuzz(numero) : 
+}
